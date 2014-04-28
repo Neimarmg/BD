11da6a22d19457805bac53cda18c324fd6439190
Aurélio Santana <aurelio.santana@terra.com.br>
2014-04-28 12:39:10 -0300

    01

--- BD2_2M/T3 - Avalicação 2014-1/Funcao - cliente_sem_bonus.sql
+++ BD2_2M/T3 - Avalicação 2014-1/Funcao - cliente_sem_bonus.sql
@@ -1,32 +1,4 @@
 ﻿
-CREATE TABLE clientes (
-	
-	idCliente serial PRIMARY KEY, 
-	cpf VARCHAR(20),
-	nome VARCHAR(50) NOT NULL,
-	endereco VARCHAR(100) NULL,
-	idCidade INTEGER NULL,
-	uf VARCHAR(02) NULL,
-	cep VARCHAR(100),
-	telefone VARCHAR(15),
-	data_cadastro DATE,
-	data_nascimento DATE,
-
-	CHECK (idCidade > 0),
-	
-	CONSTANT FKIdCidades FOREIGN key idCidade REFERENCES cidades(idCidade)
-	
-);
-
-CREATE TABLE bonus {
-	codigo_bonus serial PRIMARY KEY, 
-	codigo_bonus INTEGER, 
-	cpf_cliente INTEGER, 
-	codigo_venda INTEGER, 
-	bonus DOUBLE PRECISION
-)
-
-
 -- Exe 18
 
 CREATE or replace FUNCTION get_cliente_sem_bonus() RETURNS TABLE (
@@ -34,13 +6,11 @@ nome VARCHAR (15),
 idCliente INTEGER) AS $$
 BEGIN
 RETURN 
-	QUERY SELECT INTO FROM *clientes outer left Join clientes.clientes = null,  
+	QUERY SELECT INTO FROM *clientes outer left Join bonus ON clientes.clientes = bonus.idCliente, 
+		SELECT INTO FROM *clientes outer left Join bonus ON bonus.codigo_bonus = null,
 		
-		bonus.codigo_bonus
-		
-		FROM clientes,bonus 
-		WHERE bonus.idCliente = 0;
-
+		WHERE bonus.idCliente = null;
+	????? 
 RETURN $$;
 END;
 $$ LANGUAGE 'plpgsql'
--- BD2_2M/T3 - Avalicação 2014-1/bonus.sql
+++ BD2_2M/T3 - Avalicação 2014-1/bonus.sql
@@ -1,8 +1,12 @@
 ﻿
 CREATE TABLE bonus {
 	codigo_bonus serial PRIMARY KEY, 
-	codigo_bonus INTEGER, 
+	codigo_bonus INTEGER,
+	idCliente INTEGER,
 	cpf_cliente INTEGER, 
 	codigo_venda INTEGER, 
 	bonus DOUBLE PRECISION
 )
+
+INSERT INTO bonus (idCliente, cpf_cliente, codigo_venda, bonus) VALUES 
+(1, 124, 1, 10,2)
